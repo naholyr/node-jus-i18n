f541ba21805206ff21be0f121583f50d7e5f55d8
Nicolas Chambrier <nchambrier@clever-age.com>
2011-01-26 13:46:06 +0100

    FIX test

--- test.js
+++
@@ -1,8 +0,0 @@
-var i18n = require('./i18n');
-
-i18n.load();
-i18n.defaultLocale = 'fr';
-i18n.debug();
-console.log(i18n.translate('guy'));
-console.log(i18n.translate('guys'));
-console.log(i18n.translate('guy', 'en'));
