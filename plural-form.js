8ab263d011457aabcea6092c5a51390e8d2b8e56
Nicolas Chambrier <naholyr@gmail.com>
2011-01-28 17:48:04 +0100

    WiP plural forms syntax

--- plural-form.js
+++ plural-form.js
@@ -1,4 +1,20 @@
-// TODO
+var RE_MESSAGE = /^((?:\[[^\[\]]+\])|(?:\{[^\{\}]+\}))(.*?)(\|(?:(?:\[[^\[\]]+\])|(?:\{[^\{\}]+\})).*)?$/g;
+
+// Plural forms separated by '|'
+var RE_PLURAL_SEPARATOR = /\|/g;
+
+// Escape '|' using '||'
+var RE_ESCAPED_SEPARATOR = /\|\|/g;
+var ENCODED_SEPARTOR = '||.';
+var RE_ENCODED_SEPARATOR = /\|\|\./g;
+var DECODED_SEPARATOR = '|';
+
+// Extract plural forms from a single string
+var extractPluralForms = function extractPluralForms(string) {
+	string = string.replace(RE_ESCAPED_SEPARATOR, ENCODED_SEPARATOR);
+	
+}
+
 module.exports = function pluralHandler(msg, number) {
 	return msg;
 };
