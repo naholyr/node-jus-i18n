05399990f4b5e7991ba91b5c2d54652e8d4999c3
Nicolas Chambrier <naholyr@gmail.com>
2011-01-30 17:02:57 +0100

    Test: better illustration of using plural forms

--- test/i18n-data/messages.js
+++ test/i18n-data/messages.js
@@ -1,6 +1,6 @@
 exports.fr = {
 	'guy': 'mec',
-	'You have %n% messages': "[0]Vous n'avez aucun message|[1]Vous avez un message|[2-+Inf]Vous avez %n% messages"
+	'You have %n% messages': "[0]Vous n'avez aucun message|[1]Vous avez un message|[2-3]Vous avez %n% messages|[-Inf-+Inf]Vous avez trop de messages"
 };
 
 exports.en = {
