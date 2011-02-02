ece2ab1f820a9becb7baa91529c7635a1a8f36d4
Nicolas Chambrier <naholyr@gmail.com>
2011-02-02 15:43:16 +0100

    Added "dummy" store, just a dumb empty store to be used as template for custom stores

---
+++ stores/dummy.js
@@ -0,0 +1,39 @@
+
+exports.load = function load(catalogue, locales, i18n, callback) {
+	// Too dumb to load anything
+	if (typeof locales == 'undefined') {
+		// No locales: load by catalogue
+		return callback({'ALL':new Error('Too dumb to load a catalogue')}, undefined, this);
+	} else {
+		// Load specified locales
+		var errors = {};
+		locales.forEach(function(locale) {
+			errors[locale] = new Error('Too dumb to load locale ' + locale);
+		});
+		errors.ALL = new Error('Too dumb to load anything');
+		callback(errors, [], this);
+	}
+};
+
+exports.get = function get(key, locale, catalogue) {
+	// Too dumb to have stored anything
+	return undefined;
+};
+
+exports.configure = function configure(options, callback) {
+	// Too dumb to be configured
+	if (typeof options != 'object') {
+		return callback(new Error('Invalid options'), this);
+	}
+	return callback(undefined, this);
+};
+
+exports.locales = function locales(prefix, catalogue, callback) {
+	// Too dumb...
+	return callback(new Error('Too dumb to know what locales I can load'), undefined, this);
+};
+
+exports.catalogues = function catalogues(callback) {
+	// Too dumb...
+	return callback(new Error('Too dumb to know what catalogues I can load'), undefined, this);
+};
